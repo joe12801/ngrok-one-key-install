30508634ba6c3bc00063bdbab2cdd91bab14e3ff
joe12801 <70465495+joe12801@users.noreply.github.com>
2021-01-19 03:13:39 +0800

    Update install_ngrok.sh

--- install_ngrok.sh
+++ install_ngrok.sh
@@ -285,8 +285,8 @@ function pre_install(){
         echo -e "ngrok status manage: ${COLOR_PINKBACK_WHITEFONT}/etc/init.d/ngrokd${COLOR_END} {${COLOR_GREEN}start${COLOR_END}|${COLOR_PINK}stop${COLOR_END}|${COLOR_YELOW}restart${COLOR_END}|${COLOR_BLUE}config${COLOR_END}|${COLOR_RED}adduser${COLOR_END}|${COLOR_GREEN}info${COLOR_END}}"
         echo -e "Your Domain: ${COLOR_GREEN}${NGROK_DOMAIN}${COLOR_END}"
         echo -e "Ngrok password: ${COLOR_GREEN}${ngrok_pass}${COLOR_END}"
-        echo -e "http_port: ${COLOR_GREEN}81${COLOR_END}"
-        echo -e "https_port: ${COLOR_GREEN}443${COLOR_END}"
+        echo -e "http_port: ${COLOR_GREEN}80${COLOR_END}"
+        echo -e "https_port: ${COLOR_GREEN}445${COLOR_END}"
         echo -e "remote_port: ${COLOR_GREEN}4443${COLOR_END}"
         echo -e "Config file:   ${COLOR_GREEN}${str_ngrok_dir}/.ngrok_config.sh${COLOR_END}"
         echo ""
@@ -315,8 +315,8 @@ cat > ${str_ngrok_dir}/.ngrok_config.sh <<EOF
 # -------------config START-------------
 dns="${NGROK_DOMAIN}"
 pass="${ngrok_pass}"
-http_port=81
-https_port=443
+http_port=80
+https_port=445
 remote_port=4443
 srtCRT=server.crt
 strKey=server.key
@@ -330,8 +330,8 @@ cat > ${str_ngrok_dir}/.ngrok_config.sh <<EOF
 # -------------config START-------------
 dns="${NGROK_DOMAIN}"
 pass="${ngrok_pass}"
-http_port=81
-https_port=443
+http_port=80
+https_port=445
 remote_port=4443
 srtCRT=server.crt
 strKey=server.key
